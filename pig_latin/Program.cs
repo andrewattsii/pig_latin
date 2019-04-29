2d6cc774529de56cb0d8b019b23f4ab06c5608a1
Andre Watts <andrewattsii@gmail.com>
2019-04-29 06:55:49 -0400

    final pig latin

--- pig_latin/Program.cs
+++ pig_latin/Program.cs
@@ -5,7 +5,7 @@ namespace pig_latin
     class Program
     {
         static void Main(string[] args)
-        {
+        { beginning:
             Console.WriteLine("Welcome to  the pig latin translator !\n \nEnter a line to be translated:");
             string sentence = Console.ReadLine();
             sentence.ToLower();
@@ -14,33 +14,52 @@ namespace pig_latin
 
             string[] words = lowerCase.Split();
             string vowels = "a" + "e" + "i" + "u";
+            
 
+            
+           
 
-            string pigLatin = "";
-            foreach (string word in words)
-            {
-                if (vowels.Contains(word[0]))
+                string pigLatin = "";
+                foreach (string word in words)
                 {
-                    pigLatin += word + "way ";
-                    Console.WriteLine(pigLatin);
-                }
-                int counter = 0;
-                string pigLatin1 = word;
-                foreach (char c in pigLatin1)
-                {
-                    if (!vowels.Contains(c))
+                    
+                    if (vowels.Contains(word[0]))
                     {
-                        counter++;
+                        pigLatin += word + "way ";
+                        Console.WriteLine(pigLatin);
+                    
                     }
-                    else
+                    int counter = 0;
+                    string pigLatin1 = word;
+                    foreach (char c in pigLatin1)
+                    {
+                        if (!vowels.Contains(c))
+                        {
+                            counter++;
+                        }
+                        else
+                            break;
+
+                        pigLatin1 = word.Substring(counter) + word.Substring(0, counter) + "ay";
+
+                    Console.WriteLine($" {pigLatin1} \n do you want to go again ? y/n ");
+                    string yesno = Console.ReadLine();
+                    if (yesno == "n")
                         break;
+                    else if (yesno == "y")
+                        goto beginning;
+                   
+                    }
 
-                    pigLatin1 = word.Substring(counter) + word.Substring(0, counter) + "ay";
-
-                    Console.Write($" {pigLatin1}");
                 }
-            }
-        }    
-        
+               
+
+            
+           
+            
+           
+        }
+
+ 
     }
 }
